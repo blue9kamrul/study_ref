3fd0f2f786651e1cd7b74c771d961352ca6bff03
cdkamrul <cdkamrul9@gmail.com>
2023-06-18 15:54:00 +0600

    h

--- searching algo/bin_search.cpp
+++ searching algo/bin_search.cpp
@@ -1,24 +1,39 @@
-#include<iostream>
-
+// C++ program to implement iterative Binary Search
+#include <iostream>
 using namespace std;
 
-int main(){
-    int a[10]={1,2,3,4,5,6,7,8,9};
+// An iterative binary search function.
+int binarySearch(int arr[], int l, int r, int x)
+{
+	while (l <= r) {
+		int m = l + (r - l) / 2;
 
-    int x=9;
+		// Check if x is present at mid
+		if (arr[m] == x)
+			return m;
 
-    int low=0;
-    int high=9;
- int mid=(low+high)/2;
-    while(low<=high){
-       
+		// If x greater, ignore left half
+		if (arr[m] < x)
+			l = m + 1;
 
-        if(a[mid]==x) return mid;
-        else if(a[mid]<x) low=mid+1;
-        else if(a[mid]>x) high=mid-1;
-        else return -1;
-    }
-    //cout<<a[mid];
+		// If x is smaller, ignore right half
+		else
+			r = m - 1;
+	}
 
+	// If we reach here, then element was not present
+	return -1;
+}
 
+// Driver code
+int main(void)
+{
+	int arr[] = { 2, 3, 4, 10, 40 };
+	int x = 10;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int result = binarySearch(arr, 0, n - 1, x);
+	(result == -1)
+		? cout << "Element is not present in array"
+		: cout << "Element is present at index " << result;
+	return 0;
 }
