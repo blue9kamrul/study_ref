73eabc497089c5be68362d66f8b90b42691493b6
cdkamrul <cdkamrul9@gmail.com>
2023-06-20 10:29:46 +0600

    bubbleSort

---
+++ sorting_algo/bubble.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<utility>
+
+using namespace std;
+
+void bubbleSort(int a[],int n){
+    int i,j;
+    for ( i = 0; i < n; i++)
+    {
+        for ( j = 0; j < n-i-1; j++)
+        {
+            if(a[j]>a[j+1]){
+                swap(a[j],a[j+1]);
+            }
+        }   
+    }   
+}
+
+void display(int a[],int n){
+	for (int i = 0; i < n; i++)
+	{
+		cout<<a[i]<<" ";
+	}	
+}
+
+int main(){
+    int a[]={45,78,12,90,76,42,35};
+	int n = sizeof(a)/sizeof(a[0]);
+
+    bubbleSort(a,n);
+    cout<<"sorted array is: "<<endl;
+
+    display(a,n);
+}
