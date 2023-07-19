08cf679b647d3b1cd52fbf4d916631bb1e68668c
cdkamrul <cdkamrul9@gmail.com>
2023-07-19 19:40:21 +0600

    bb

---
+++ sorting_algo/bubble-normal.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int a[], int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if(a[j]>a[j+1]){
+                int temp=a[j+1];
+                a[j+1]=a[j];
+                a[j]=temp;
+            }
+        }
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    
+    
+}
+
+int main(){
+    int a[]={4,2,6,0,2,5,8};
+    int n= sizeof(a)/sizeof(a[0]);
+
+    bubbleSort(a,n);
+
+    return 0;
+}
